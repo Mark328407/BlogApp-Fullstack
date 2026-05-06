20cc8281b6132f551e985ca12a8cdc138e93f3e3
markoestrecho <markoestrecho@gmail.com>
2026-05-06 20:30:17 +0800

    Add Changes in Activity

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://blogapp-fullstack-az9o.onrender.com/api',
+  baseURL: 'https://blogapp-fullstack-az9o.onrender.com/',
 })
 
 
